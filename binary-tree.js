e759b505ea1982c253c840b8e72ec7900870d332
Lonsper <Lonsper@gmail.com>
2021-04-21 16:15:07 +0200

    Create binary-tree.js
    Calculate sum, average and median of subtree with sample

---
+++ binary-tree.js
@@ -0,0 +1,134 @@
+const side = {
+    LEFT : 0,
+    RIGHT : 1,
+    BACK : 2
+}
+
+class Node {
+    constructor(value, parent){
+        this.value = value;
+        this.parent = parent;
+        this.nextLeft = null;
+        this.nextRight = null;
+    }
+}
+
+class BinaryTree {
+    constructor(rootValue) {
+        this.root = new Node(rootValue, null);
+        this.cursor = this.root;
+        this.nodesValue = [];
+    }
+
+    addNode(value, side) {
+        switch (side) {
+            case 0 : this.cursor.nextLeft = new Node(value, this.cursor);
+                break;
+            case 1 : this.cursor.nextRight = new Node(value, this.cursor);
+                break;
+            default : console.log('Wrong side - addNode() error!');
+        }
+    }
+
+    moveCursor(side) {
+        switch (side) {
+            case 0 : this.cursor = this.cursor.nextLeft;
+                break;
+            case 1 : this.cursor = this.cursor.nextRight;
+                break;
+            case 2 : this.cursor = this.cursor.parent;
+                break;
+            default : console.log('Wrong direction - moveCursor() error!');
+        }
+    }
+
+    printTree() {
+        this.printAllNodes(this.root);
+    }
+
+    printSubtree(cursorNode) {
+        this.printAllNodes(cursorNode);
+    }
+
+    printAllNodes(node) {
+        if (node != null) {
+            process.stdout.write(node.value + ' ');
+    
+            if (node.nextLeft != null) 
+                this.printAllNodes(node.nextLeft);
+            
+            if (node.nextRight != null)
+                this.printAllNodes(node.nextRight);
+        } else 
+            return;
+    }
+
+    prepareNodesArray(){
+        this.nodesValue = [];
+    }
+
+    calculateSubtreeValues(cursorNode, currentSum){
+        if(cursorNode != null){
+            if(cursorNode.nextLeft != null)
+                currentSum = this.calculateSubtreeValues(cursorNode.nextLeft, currentSum);
+
+            if(cursorNode.nextRight != null)
+                currentSum = this.calculateSubtreeValues(cursorNode.nextRight, currentSum);
+
+            this.nodesValue.push(cursorNode.value);
+            return currentSum += cursorNode.value;
+        } else {
+            return currentSum += 0;
+        }
+    }
+
+    calculateAverageSubtree(sum){
+        return Math.round(sum/this.nodesValue.length * 100) / 100;
+    }
+
+    calculateMedianSubtree(){
+        const midValue = Math.floor(this.nodesValue.length / 2),
+        sortedValues = [...this.nodesValue].sort((a, b) => a - b);
+        return this.nodesValue.length % 2 !== 0 ? sortedValues[midValue] : (sortedValues[midValue - 1] + sortedValues[midValue]) / 2;
+    }  
+
+    printSubtreeValues(cursorNode) {
+        this.prepareNodesArray();
+        var sum = this.calculateSubtreeValues(cursorNode, 0);
+        var average = this.calculateAverageSubtree(sum);
+        var median = this.calculateMedianSubtree();
+
+        console.log(
+            '\nSum of subtree values - ' + sum,
+            '\nAverage of subtree values - ' + average,
+            '\nMedian of subtree values - ' + median
+        )
+    }
+}
+
+let bt = new BinaryTree(5);
+bt.addNode(3, side.LEFT);
+bt.addNode(7, side.RIGHT);
+
+bt.moveCursor(side.LEFT);
+bt.addNode(2, side.LEFT);
+bt.addNode(5, side.RIGHT);
+
+bt.moveCursor(side.BACK);
+bt.moveCursor(side.RIGHT);
+bt.addNode(1, side.LEFT);
+bt.addNode(0, side.RIGHT);
+
+bt.moveCursor(side.RIGHT);
+bt.addNode(2, side.LEFT);
+bt.addNode(8, side.RIGHT);
+
+bt.moveCursor(side.RIGHT);
+bt.addNode(5, side.RIGHT);
+
+bt.moveCursor(side.BACK);
+bt.moveCursor(side.BACK);
+bt.moveCursor(side.BACK); // Cursor shows on root location
+
+bt.printSubtree(bt.cursor);
+bt.printSubtreeValues(bt.cursor);
