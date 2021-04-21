e427dcbc3b9ba63b937d35b994b9e41dcc61f77b
Lonsper <Lonsper@gmail.com>
2021-04-21 17:52:00 +0200

    Added template for unit tests
    unit test for enum side

---
+++ test/binary-tree.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const BinaryTree = require('../binary-tree');
+
+describe('Enum Side Test', () => {
+    it('side.LEFT equal 0', () => {
+        assert.equal(side.LEFT, 0);
+    });
+    it('side.RIGHT equal 1', () => {
+        assert.equal(side.RIGHT, 1);
+    });
+    it('side.BACK equal 2', () => {
+        assert.equal(side.BACK, 2);
+    })
+   });
